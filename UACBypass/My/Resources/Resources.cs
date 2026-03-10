08e1a99b73535db893ddc0f56060fb960012044a
Bennett82r <p.agi@mail.ru>
2026-03-10 22:19:23 +0400

    Upload Files

---
+++ UACBypass/My/Resources/Resources.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace Plugin.My.Resources
+{
+  [StandardModule]
+  [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+  [DebuggerNonUserCode]
+  [CompilerGenerated]
+  [HideModuleName]
+  internal sealed class Resources
+  {
+    private static ResourceManager resourceMan;
+    private static CultureInfo resourceCulture;
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static ResourceManager ResourceManager
+    {
+      get
+      {
+        if (object.ReferenceEquals((object) Plugin.My.Resources.Resources.resourceMan, (object) null))
+          Plugin.My.Resources.Resources.resourceMan = new ResourceManager("Plugin.Resources", typeof (Plugin.My.Resources.Resources).Assembly);
+        return Plugin.My.Resources.Resources.resourceMan;
+      }
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static CultureInfo Culture
+    {
+      get => Plugin.My.Resources.Resources.resourceCulture;
+      set => Plugin.My.Resources.Resources.resourceCulture = value;
+    }
+  }
+}
