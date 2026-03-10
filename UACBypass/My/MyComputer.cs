0fdc1662026d0b662c13935b5fb8418e59325651
Bennett82r <p.agi@mail.ru>
2026-03-10 22:19:16 +0400

    Upload Files

---
+++ UACBypass/My/MyComputer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic.Devices;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Plugin.My
+{
+  [GeneratedCode("MyTemplate", "11.0.0.0")]
+  [EditorBrowsable(EditorBrowsableState.Never)]
+  internal class MyComputer : Computer
+  {
+    [DebuggerHidden]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public MyComputer()
+    {
+    }
+  }
+}
