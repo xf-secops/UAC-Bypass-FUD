5fd5656720c3027d9c49dfc99fcc8262e9018ca9
Bennett82r <p.agi@mail.ru>
2026-03-10 22:19:06 +0400

    Upload Files

---
+++ UACBypass/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UACBypass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UACBypass")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("E68ED988-2C13-4637-9B4D-8237161AC987")]
+[assembly: AssemblyFileVersion("2.1.689.5943")]
+[assembly: AssemblyVersion("2.1.689.5943")]
