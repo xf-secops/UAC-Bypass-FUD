2253b6bdecfb85849e37aeabce44f4b6471abeef
Bennett82r <p.agi@mail.ru>
2026-03-10 22:19:20 +0400

    Upload Files

---
+++ UACBypass/My/MySettings.cs
@@ -0,0 +1,24 @@
+﻿using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace Plugin.My
+{
+  [CompilerGenerated]
+  [GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  internal sealed class MySettings : ApplicationSettingsBase
+  {
+    private static MySettings defaultInstance = (MySettings) SettingsBase.Synchronized((SettingsBase) new MySettings());
+
+    public static MySettings Default
+    {
+      get
+      {
+        MySettings defaultInstance = MySettings.defaultInstance;
+        return defaultInstance;
+      }
+    }
+  }
+}
